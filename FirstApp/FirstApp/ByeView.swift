da5f00868d2427ffd8eead65bd557253142b824d
Kenyon <kengeet56@gmail.com>
2023-01-17 12:06:16 -0600

    chapter 1

---
+++ FirstApp/FirstApp/ByeView.swift
@@ -0,0 +1,20 @@
+//
+//  ByeView.swift
+//  FirstApp
+//
+//  Created by Brenden A Mudd on 1/17/23.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text("Bye bye, World!")
+    }
+}
+
+struct ByeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ByeView()
+    }
+}
