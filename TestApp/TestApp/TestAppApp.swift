e29f33b7a942d929da838a545ac2c51eff00944e
Kenyon <kengeet56@gmail.com>
2023-01-17 12:01:44 -0600

    init commit

---
+++ TestApp/TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Brenden A Mudd on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
