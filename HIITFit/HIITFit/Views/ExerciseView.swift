da78fe2368e03bf8053d94ed607446ed28c206c3
Kenyon <kengeet56@gmail.com>
2023-01-20 10:58:44 -0600

    changed gitignore

--- HIITFit/HIITFit/Views/ExerciseView.swift
+++ HIITFit/HIITFit/Views/ExerciseView.swift
@@ -5,12 +5,36 @@ struct ExerciseView: View {
     let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
     let index: Int
     var body: some View {
-        Text(exerciseNames[index])
+        VStack {
+            HeaderView()
+            Text("Video player")
+            Text("Timer")
+            Text("Start/Done button")
+            Text("Rating")
+            Text("History button")
+        }
     }
+    
 }
 
 struct ExerciseView_Previews: PreviewProvider {
     static var previews: some View {
         ExerciseView(index: 0)
+    }
+}
+
+struct HeaderView: View {
+    var body: some View {
+        VStack {
+            Text(exerciseNames[index])
+                .font(.largeTitle)
+            HStack {
+                Image(systemName: "1.circle")
+                Image(systemName: "2.circle")
+                Image(systemName: "3.circle")
+                Image(systemName: "4.circle")
+            }
+            .font(.title2)
+        }
     }
 }
