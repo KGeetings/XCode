134c7e1b1055ff309a6fbcd42557a05dc8cd277b
Kenyon <kengeet56@gmail.com>
2023-01-31 10:00:05 -0600

    chapter 9 stuff

--- HIITFit/HIITFit/Model/HistoryStore.swift
+++ HIITFit/HIITFit/Model/HistoryStore.swift
@@ -10,14 +10,14 @@ class HistoryStore: ObservableObject {
   @Published var exerciseDays: [ExerciseDay] = []
     init() {
       #if DEBUG
-      createDevData()
+      //createDevData()
       #endif
     }
     func addDoneExercise(_ exerciseName: String) {
       let today = Date()
-      if today.isSameDay(as: exerciseDays[0].date) { // 1
-        print("Adding \(exerciseName)")
-        exerciseDays[0].exercises.append(exerciseName)
+        if let firstDate = exerciseDays.first?.date {
+            print("Adding \(exerciseName)")
+            exerciseDays[0].exercises.append(exerciseName)
       } else {
         exerciseDays.insert( // 2
         ExerciseDay(date: today, exercises: [exerciseName]),
