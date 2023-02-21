11db2db9695fd8d467c217f3b0d9a53bcb51b7b4
Kenyon <kengeet56@gmail.com>
2023-02-21 13:43:10 -0600

    New Cards app

---
+++ Cards/Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Kenyon on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
