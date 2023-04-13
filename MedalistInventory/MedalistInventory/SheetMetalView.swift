e36d426784771c4443902de632b3c14283da089b
Kenyon <kengeet56@gmail.com>
2023-04-13 18:48:18 -0500

    attempt at add new row to database

--- MedalistInventory/MedalistInventory/SheetMetalView.swift
+++ MedalistInventory/MedalistInventory/SheetMetalView.swift
@@ -190,8 +190,43 @@ struct AddTableRowView: View {
                     TextField("Allocated", value: $allocated, formatter: NumberFormatter())
                 }
                 Button(action: {
-                    let newRow = TableData(id: id, material: material, thickness: thickness, length: Double(length), width: Double(width), quantity: quantity, allocated: allocated)
-                    tableData.append(newRow)
+                    // Check if Length, Width, Quantity, and Allocated are all positive numbers
+                    guard length > 0 && width > 0 && quantity > 0 && allocated > 0 else {
+                        print("Length, Width, Quantity, and Allocated must all be positive numbers")
+                        return
+                    }
+
+                    // Check if Material and Thickness are not empty
+                    guard !material.isEmpty && !thickness.isEmpty else {
+                        print("Material and Thickness must not be empty")
+                        return
+                    }
+
+                    // Check if length is greater or equal to width
+                    guard length >= width else {
+                        print("Length must be greater than or equal to Width")
+                        return
+                    }
+
+                    // Use database_query.php to add a new row to the database
+                    let url = URL(string: "http://10.0.2.3/database_query_mobileapps.php")!
+                    var request = URLRequest(url: url)
+                    request.httpMethod = "POST"
+                    //data: {task: 'insert', schema: 'sheet_metal_inventory', material: document.getElementById("material-select").value, thickness: document.getElementById("thickness-select").value, length: document.getElementById("length-input").value, width: document.getElementById("width-input").value, quantity: document.getElementById("quantity-input").value},
+                    let postString = "taks=insert&schema=sheet_metal_inventory&material=\(material)&thickness=\(thickness)&length=\(length)&width=\(width)&quantity=\(quantity)"
+                    request.httpBody = postString.data(using: .utf8)
+                    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                        guard let data = data, error == nil else {
+                            print(error?.localizedDescription ?? "No data")
+                            return
+                        }
+                        let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+                        if let responseJSON = responseJSON as? [String: Any] {
+                            print(responseJSON)
+                        }
+                    }
+                    task.resume()
+
                     isPresented = false
                 }, label: {
                     Text("Add Row")
@@ -237,17 +272,10 @@ struct EditTableRowView: View {
                     TextField("Allocated", value: $tableDataToEdit.allocated, formatter: NumberFormatter())
                 }
                 Button(action: {
-//                    // Check if Length, Width, Quantity, and Allocated are all numbers
-//                    guard let _ = Double(tableDataToEdit.length) else { return }
-//                    guard let _ = Double(tableDataToEdit.width) else { return }
-//                    guard let _ = Int(tableDataToEdit.quantity) else { return }
-//                    guard let _ = Int(tableDataToEdit.allocated) else { return }
-
                     // Check if Length, Width, Quantity, and Allocated are all positive numbers
                     guard tableDataToEdit.length > 0 && tableDataToEdit.width > 0 else { return }
                     guard tableDataToEdit.length < 125 && tableDataToEdit.width < 125 else { return }
                     guard tableDataToEdit.quantity >= 0 else { return }
-                    guard tableDataToEdit.allocated >= 0 else { return }
 
                     // Check if Lenght is greater than or equal to Width
                     guard tableDataToEdit.length >= tableDataToEdit.width else { return }
