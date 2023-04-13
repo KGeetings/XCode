2ebaa1b47e4d6f71a9c5fcb5096d5b7be4f6c3fa
Kenyon <kengeet56@gmail.com>
2023-04-13 18:31:10 -0500

    working edit button to database

--- MedalistInventory/MedalistInventory/SheetMetalView.swift
+++ MedalistInventory/MedalistInventory/SheetMetalView.swift
@@ -237,23 +237,23 @@ struct EditTableRowView: View {
                     TextField("Allocated", value: $tableDataToEdit.allocated, formatter: NumberFormatter())
                 }
                 Button(action: {
-                    // Check if Length, Width, Quantity, and Allocated are all numbers
-                    guard let _ = Double(tableDataToEdit.length) else { return }
-                    guard let _ = Double(tableDataToEdit.width) else { return }
-                    guard let _ = Int(tableDataToEdit.quantity) else { return }
-                    guard let _ = Int(tableDataToEdit.allocated) else { return }
+//                    // Check if Length, Width, Quantity, and Allocated are all numbers
+//                    guard let _ = Double(tableDataToEdit.length) else { return }
+//                    guard let _ = Double(tableDataToEdit.width) else { return }
+//                    guard let _ = Int(tableDataToEdit.quantity) else { return }
+//                    guard let _ = Int(tableDataToEdit.allocated) else { return }
 
                     // Check if Length, Width, Quantity, and Allocated are all positive numbers
-                    guard tableDataToEdit.length > 0 else { return }
-                    guard tableDataToEdit.width > 0 else { return }
-                    guard tableDataToEdit.quantity > 0 else { return }
-                    guard tableDataToEdit.allocated > 0 else { return }
+                    guard tableDataToEdit.length > 0 && tableDataToEdit.width > 0 else { return }
+                    guard tableDataToEdit.length < 125 && tableDataToEdit.width < 125 else { return }
+                    guard tableDataToEdit.quantity >= 0 else { return }
+                    guard tableDataToEdit.allocated >= 0 else { return }
 
                     // Check if Lenght is greater than or equal to Width
                     guard tableDataToEdit.length >= tableDataToEdit.width else { return }
 
                     // Use database_query.php to update the row in the database
-                    let url = URL(string: "http://10.0.2.3/database_query.php")!
+                    let url = URL(string: "http://10.0.2.3/database_query_mobileapps.php")!
                     var request = URLRequest(url: url)
                     request.httpMethod = "POST"
                     let postString = "task=update&schema=sheet_metal_inventory&id=\(tableDataToEdit.id)&material=\(tableDataToEdit.material)&thickness=\(tableDataToEdit.thickness)&length=\(tableDataToEdit.length)&width=\(tableDataToEdit.width)&quantity=\(tableDataToEdit.quantity)&allocated=\(tableDataToEdit.allocated)"
